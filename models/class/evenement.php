677a99fdc7a5970b14aac41188e7bcb27c0d6143
yoann_p <pencoley44@gmail.com>
2023-09-04 16:20:01 +0200

    add function

--- models/class/evenement.php
+++ models/class/evenement.php
@@ -10,6 +10,37 @@ class Evenement {
     public $date_fin;
     public $lieu;
 
-    public function __construct(int $id, string $titre, string $description, float $prix, string $date_debut , string $date_fin, Lieu $lieu);
+    public function __construct(int $id, string $titre, string $description, float $prix, string $date_debut , string $date_fin, Lieu $lieu) {
+        
+        $this->id = $id;
+        $this->titre = $titre;
+        $this->description = $description;
+        $this->prix = $prix;
+        $this->date_debut = $date_debut;
+        $this->date_fin = $date_fin;
+        $this->lieu = $lieu;
+
+    }
+
+    public function get_id(): int {
+
+        return $this->id;
+
+    }
+
+    public function set_id(int $id) {
+        
+        $this->id = $id;
+
+    }
+
+    public function get_titre(string $titre) {
+        
+        return $this->titre;
+
+    }
+
+    public function 
+
 }
 ?>
