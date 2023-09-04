5eed201a085f6f8127874e58d2691283e46c2a9e
yoann_p <pencoley44@gmail.com>
2023-09-04 22:05:11 +0200

    add css and js

--- index.php
+++ index.php
@@ -1,46 +1,64 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title></title>
+        <link rel="stylesheet" href="views\style.css">
+        <link rel="icon" href="" />
+    </head>
+    <body>
+        <header class="header">
+            <nav>
+                <img class="logo" src="public\images\logo_tour_eiffel.jpg">
+                <div class="main-navlinks">
+                    <button type="button" class="hamburger open" aria-label="Toggle Navigation" aria-expanded="true">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </button>
+                    <div class="navlinks-container open" style="transition: transform 0.4s ease-out 0s;">
+                        <a href="#" aria-current="page">Accueil</a>
+                        <a href="#">Lieu</a>
+                        <a href="#">Evenement</a>
+                        <a href="#">Contact</a>
+                    </div>
+                </div>
+            </nav>
+        </header>
+        <main>
+        
+            <?php
+            /*
+            $username = "cdc";
+            $password = "cdc";
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title></title>
-    <link rel="stylesheet" href="style/styles.css">
-    <link rel="icon" href="" />
-</head>
+            try {
+                $db = new PDO("pgsql:host=192.168.222.86;port=5432;dbname=pariscap;", $username, $password);
+            } catch (PDOException $e) {
+                print("Erreur connexion " . $e->getMessage());
+            }
+            switch (@$_GET['s']) {
+                default:
+                    // include 'views/home.html';
+                    $reqLieu = $db->query("SELECT * FROM lieu ");
+                    while ($ligne = $reqLieu->fetch()) {
+                        print("<li>" . "Lieu : " . $ligne['nom'] . "</li>");
 
-<body>
-    <header>
-    </header>
+                    }
+                    break;
+            }
 
-    <main>
-        <?php
-        require('models/dbconfig.php');
-        switch (@$_GET['s']) {
-            default:
-                // include 'views/home.html';
-                $reqLieu = $db->query("SELECT * FROM lieu ");
-                while ($ligne = $reqLieu->fetch()) {
-                    print("<li>" . "Lieu : " . $ligne['nom'] . "</li>");
+            if (@$_GET['s'] != "home" and @$_GET['s'] != null) {
+                echo "<a href='?s=home'>Page d'accueil</a>";
+            }
+    */
+            ?>
 
-                }
-                ;
-                break;
-        }
-
-        if (@$_GET['s'] != "home" and @$_GET['s'] != null) {
-            echo "<a href='?s=home'>Page d'accueil</a>";
-        }
-
-
-        ?>
-
-    </main>
-
-    <footer>
-
-    </footer>
-</body>
-
+        </main>
+        <footer>
+        </footer>
+        <script src="models\script.js"></script>
+    </body>
 </html>
