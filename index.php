7db83335b8cba6d31bb9fcfa852151dc67962dca
Yayathic <yanis.benlalam@yahoo.com>
2023-09-05 17:35:36 +0200

    add event page and details

--- index.php
+++ index.php
@@ -33,53 +33,26 @@
         require("models/config/config.php");
         require("models/class/lieu.php");
         require("models/fonction/fonctions_bdd.php");
+        require("models/fonction/fonctions.php");
+
 
         switch (@$_GET['s']) {
             case "home":
                 echo ("Page d'accueil");
                 break;
             case "lieux":
-                $lesLieux = getLieuxObject();
-
-                echo ("<div class='cards-container'>");
-                foreach ($lesLieux as $lieu) {
-                    echo ("<div class='card_lieu'>");
-                    echo "<h2 class='nom-lieu'>" . $lieu->get_nom() . "</h2>" . "<img class='lieu-image' src='public\images\\" . $lieu->get_image() . "'><p class='courte-description'>" . "Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti porro, eum nam ut vitae, itaque odit, quis maiores ab cupiditate aspernatur eveniet tempore error et! Id pariatur quisquam distinctio quo excepturi animi iure dolor impedit velit odit. Reprehenderit quis mollitia accusamus aliquid, libero delectus. Tempora ratione ut id et omnis!" . "</p>";
-                    afficherPlus($lieu->get_id());
-                    echo ("</div>");
-                    /*foreach ($reqDesservir->fetchAll(PDO::FETCH_ASSOC) as $idTransport) {
-                        $idTrans = $idTransport['idtransport'];
-
-                        array_push($transportIds, $idTrans);
-
-                        $reqTransports = $db->query("SELECT * FROM transport where id = $idTrans");
-                        foreach ($reqTransports->fetchAll(PDO::FETCH_ASSOC) as $transport) {
-                            $idTrans = $transport['id'];
-                            $arretTrans = $transport['arret'];
-                            echo " " . $idTrans . " à l'arrêt : " . $arretTrans;
-
-                        }
-
-                    }*/
-                    echo "<br>";
-
-                    // $lieu = new Lieu($lieu['id'], $lieu['nom'], $lieu['description']);
-        
-                }
-                echo ("</div>");
-                $reqDesservir = $db->query("SELECT idtransport FROM desservir WHERE idlieu = 2");
-                // var_dump($reqDesservir->fetchAll(PDO::FETCH_ASSOC));
-        
-
-
-
-                // var_dump($lieu);
+                displayLieux();
                 break;
             case "evenement":
+                displayEvents();
                 break;
             case "contact":
                 break;
             case "lieu":
+                break;
+            case "event";
+                caseEventHandler($_GET['idE']);
+                break;
 
         }
         ?>
