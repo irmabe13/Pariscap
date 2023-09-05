d5c9b088cba774c0c8116f0d23a85c832c42e3c7
jimiab <jim.tillocher@gmail.com>
2023-09-05 16:20:22 +0200

    fonction est un lieu

--- models/fonction/fonctions.php
+++ models/fonction/fonctions.php
@@ -2,12 +2,7 @@
 
 function est_un_lieu(string $ref_page):bool {
         
-    if (substr($ref_page, 0, 5) == "lieu_"){
-        return True;
-    }
-    else{
-        return False;
-    }
+    return substr($ref_page, 0, 5) == "lieu_";
 
 }
 
@@ -15,7 +10,7 @@ assert(est_un_lieu("lieu_10"));
 assert(!est_un_lieu("lieu0"));
 assert(!est_un_lieu(12));
 assert(!est_un_lieu(""));
-
+assert(!est_un_lieu("lieu"))
 
 
 ?>
