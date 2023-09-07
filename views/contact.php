bdbcc5216b79d8217490badc9e079e8ad1bceae8
yoann_p <pencoley44@gmail.com>
2023-09-07 15:58:28 +0200

    change config.php

--- views/contact.php
+++ views/contact.php
@@ -1,3 +1,7 @@
+<div class="contact-container">
+    <input type='text' placeholder='Objet du message'>
+    <input type='text' placeholder='Écrivez votre message içi'>
+</div>
 <?php
-echo('page contact');
+
 ?>
